ddd589ade1080ef7d31fcb807afd06836b69bef7
Sova-RGX <arshav2006@gmail.com>
2026-07-20 11:41:58 +0530

    Program 003: SUM , Difference , Product , Quotient

---
+++ sum_diff_prod_quo.py
@@ -0,0 +1,11 @@
+a=int(input("Enter Num1: "))
+b=int(input("Enter Num2: "))
+sum_of_numbers=a+b
+difference=a-b
+product=a*b
+quotient=a/b
+
+print("Sum is:",sum_of_numbers)
+print("Difference is:",difference)
+print("Product is:",product)
+print("Quotient is:",quotient)
