d381df0064512341e83829c589cbb7366f76edc6
Sova-RGX <arshav2006@gmail.com>
2026-07-25 20:51:36 +0530

    Program 006:Swapping two numbers using a third variable

---
+++ Swapping two  number.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the first number a: "))
+b=int(input("Enter the second number b: "))
+c=a
+a=b
+b=c
+print("The first number after swapping a is:", a)
+print("The second number after swapping b is:", b)
