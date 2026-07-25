db8c2849b988aa87c122ac1a2acd4680df4a3211
Sova-RGX <arshav2006@gmail.com>
2026-07-25 14:18:39 +0530

    Program 004: Area & Circumference of a Cirlce

---
+++ Circle_Area_Circumference.py
@@ -0,0 +1,5 @@
+R=float(input("Enter the radius of the circle: "))
+area=3.14*R**2
+circumference=2*3.14*R
+print("Area of the circle is:",area)
+print("Circumference of the circle is:",circumference)
