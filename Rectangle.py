97503ed5e31cae28de42d7ac3e61d4076a0c31ed
Sova-RGX <arshav2006@gmail.com>
2026-07-25 20:45:28 +0530

    Program 005: Area & Perimeter of a Rectangle

---
+++ Rectangle.py
@@ -0,0 +1,6 @@
+A=float(input("Enter the length of the rectangle: "))
+B=float(input("Enter the width of the rectangle: "))    
+C=A*B               
+print("The area of the rectangle is:", C)  
+P=2*(A+B)
+print("The perimeter of the rectangle is:", P)  
