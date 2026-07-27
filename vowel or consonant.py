f85d30b8914ad214795d88864ca83a008d285a8e
Sova-RGX <arshav2006@gmail.com>
2026-07-27 14:32:30 +0530

    Program 016: Vowel OR Consonant

---
+++ vowel or consonant.py
@@ -0,0 +1,5 @@
+entry=str(input("Enter a character : "))
+if (entry=='a' or entry=='e' or entry=='i' or entry=='o' or entry=='u'):
+    print(entry, "is a vowel")
+else:
+    print(entry, "is a consonant")
