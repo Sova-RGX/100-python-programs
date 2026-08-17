413e3ee91eb602bb922c2edf31570e6d9dabc41b
Sova-RGX <arshav2006@gmail.com>
2026-08-17 22:22:29 +0530

    Program 020:Voter age check

--- Voting criteria check.py
+++ Voting criteria check.py
@@ -2,4 +2,4 @@ age=int(input("Enter the age of the candidate:"))
 if age>=18:
     print("The candidate is eligible to vote !")
 else:
-    print("The candidate is not eligible to vote !")
+    print("The candidate is not eligible to vote child!")
